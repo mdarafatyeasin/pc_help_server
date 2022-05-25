bcdda03cf4c676cea60e56a0240f1a1c6fdfc2fd
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-25 19:31:10 +0600

    mongodb connected successfully

--- index.js
+++ index.js
@@ -3,12 +3,34 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port =process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xowczx7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    try{
+        await client.connect();
+        console.log('database connected')
+    }
+    finally{
+
+    }
+
+}
+
+
+
+run().catch(console.dir);
+// --------------------------------------------------------
 app.get('/', (req, res) => {
   res.send('Hello From PC help!')
 })
