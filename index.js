746c6facbaca981368742ce533fa215b3c124227
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-29 18:24:32 +0600

    jwt token issu solved

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -88,7 +89,8 @@ async function run() {
                 $set: user,
               };
               const result = await usersCollection.updateOne(filter, updateDoc, options);
-              res.send(result)
+              const token =jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRETE, {expiresIn: '1h'})
+              res.send({result,token})
         })
 
     }
